23ebf38510e13d57f9e04e309612249c0c7e252b
drexpp <ivandega22@gmail.com>
2018-02-27 20:53:32 +0100

    Item 6

---
+++ Item 6/Item 6/Item 6/Script-creacion.sql
@@ -0,0 +1,10 @@
+drop database if exists `Sample-Project`;
+create database `Sample-Project`;
+
+grant select, insert, update, delete
+on `Sample-Project`.* to 'acme-user'@'%';
+
+grant select, insert, update, delete, create, drop, references, index, alter,
+create temporary tables, lock tables, create view, create routine, 
+alter routine, execute, trigger, show view
+on `Sample-Project`.* to 'acme-manager'@'%';
